1207bb155ba93df5dbf7eab8116a6135c2e81e24
Nguyen Duc Trinh <trinhcu18@gmail.com>
2016-12-07 06:35:26 +0700

    Các lớp Manager

---
+++ src/mediaone/BookManager.java
@@ -0,0 +1,93 @@
+package mediaone;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class BookManager {
+    private ArrayList<Book> bookList;
+    private Comparator<Product> nameOrderDown = new Comparator<Product>() {
+
+        @Override
+        public int compare(Product o1, Product o2) {
+            return o1.getName().compareTo(o2.getName());
+        }
+    };
+
+    private Comparator<Product> nameOrderUp = new Comparator<Product>() {
+
+        @Override
+        public int compare(Product o1, Product o2) {
+            return o2.getName().compareTo(o1.getName());
+        }
+    };
+
+    public BookManager() {
+        this.bookList = new ArrayList<>();
+    }
+
+    public void addItem(String name, int quantity, int price, String author, String publisher) {
+        bookList.add(new Book(name, quantity, price, author, publisher));
+    }
+
+    public void delItem(String bookName) {
+        for (int i = 0; i < bookList.size(); i++) {
+            if (bookList.get(i).getName().equals(bookName)) {
+                bookList.remove(i);
+            }
+        }
+    }
+
+    // Tim cac sach theo ten
+    public ArrayList<Product> searchItem(String productName) {
+        ArrayList<Product> searchList = new ArrayList<>();
+        String str1 = productName.toUpperCase();
+        for (int i = 0; i < bookList.size(); i++) {
+            String strI = bookList.get(i).getName().toUpperCase();
+            int mark = 0;
+            boolean check = true;
+            for (int j = 0; j < str1.length(); j++) {
+                if ((strI.indexOf(str1.codePointAt(j), mark)) == -1) {
+                    check = false;
+                    break;
+                } else {
+                    mark = strI.indexOf(str1.codePointAt(j), mark) + 1;
+                }
+            }
+            if (check)
+                searchList.add(bookList.get(i));
+        }
+        return searchList;
+    }
+
+    // Tim cac sach theo ten tac gia
+    public ArrayList<Book> searchAuthor(String authorName) {
+        ArrayList<Book> searchAuthorList = new ArrayList<>();
+        for (int i = 0; i < bookList.size(); i++) {
+            if (bookList.get(i).getAuthor().equals(authorName)) {
+                searchAuthorList.add(bookList.get(i));
+            }
+        }
+        return searchAuthorList;
+    }
+
+    // Tim cac sach theo ten nha xuat ban
+    public ArrayList<Book> searchPublisher(String publisherName) {
+        ArrayList<Book> searchPublisherList = new ArrayList<>();
+        for (int i = 0; i < bookList.size(); i++) {
+            if (bookList.get(i).getPublisher().equals(publisherName)) {
+                searchPublisherList.add(bookList.get(i));
+            }
+        }
+        return searchPublisherList;
+    }
+
+    // Sap xep
+    public ArrayList<Book> sortItem(int tieuChi) {
+        ArrayList<Book> orderedList = bookList;
+        if (tieuChi == -1)
+            orderedList.sort(nameOrderDown);
+        if (tieuChi == 1)
+            orderedList.sort(nameOrderUp);
+        return orderedList;
+    }
+}
---
+++ src/mediaone/MovieManager.java
@@ -0,0 +1,94 @@
+package mediaone;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class MovieManager {
+    private ArrayList<Movie> movieList;
+    private Comparator<Product> nameOrderDown = new Comparator<Product>() {
+
+        @Override
+        public int compare(Product o1, Product o2) {
+            return o1.getName().compareTo(o2.getName());
+        }
+    };
+
+    private Comparator<Product> nameOrderUp = new Comparator<Product>() {
+
+        @Override
+        public int compare(Product o1, Product o2) {
+            return o2.getName().compareTo(o1.getName());
+        }
+    };
+
+    public MovieManager() {
+        this.movieList = new ArrayList<>();
+    }
+
+    public void addItem(ArrayList<String> actors, String director, String name, int quantity, int price) {
+        movieList.add(new Movie(actors, director, name, quantity, price));
+    }
+
+    // Xoa san pham theo ten
+    public void delItem(String productName) {
+        for (int i = 0; i < movieList.size(); i++) {
+            if (movieList.get(i).getName().equals(productName)) {
+                movieList.remove(i);
+            }
+        }
+    }
+
+    // Tim cac phim theo ten (Cach tim giong nhu search ten Champ trong LoL)
+    public ArrayList<Product> searchItem(String productName) {
+        ArrayList<Product> searchList = new ArrayList<>();
+        String str1 = productName.toUpperCase();
+        for (int i = 0; i < movieList.size(); i++) {
+            String strI = movieList.get(i).getName().toUpperCase();
+            int mark = 0;
+            boolean check = true;
+            for (int j = 0; j < str1.length(); j++) {
+                if ((strI.indexOf(str1.codePointAt(j), mark)) == -1) {
+                    check = false;
+                    break;
+                } else {
+                    mark = strI.indexOf(str1.codePointAt(j), mark) + 1;
+                }
+            }
+            if (check)
+                searchList.add(movieList.get(i));
+        }
+        return searchList;
+    }
+
+    // Tim cac sach theo ten dao dien
+    public ArrayList<Movie> searchDirector(String directorName) {
+        ArrayList<Movie> searchDirectorList = new ArrayList<>();
+        for (int i = 0; i < movieList.size(); i++) {
+            if (movieList.get(i).getDirector().equals(directorName)) {
+                searchDirectorList.add(movieList.get(i));
+            }
+        }
+        return searchDirectorList;
+    }
+
+    // Tim cac sach theo ten dien vien
+    public ArrayList<Movie> searchActor(String actorName) {
+        ArrayList<Movie> searchActorList = new ArrayList<>();
+        for (int i = 0; i < movieList.size(); i++) {
+            if (movieList.get(i).getActors().contains(actorName)) {
+                searchActorList.add(movieList.get(i));
+            }
+        }
+        return searchActorList;
+    }
+
+    // Sap xep
+    public ArrayList<Movie> sortItem(int tieuChi) {
+        ArrayList<Movie> orderedList = movieList;
+        if (tieuChi == -1)
+            orderedList.sort(nameOrderDown);
+        if (tieuChi == 1)
+            orderedList.sort(nameOrderUp);
+        return orderedList;
+    }
+}
