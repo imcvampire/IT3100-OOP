4bb7bd9f09d20432d9b68d0c65c141de17b00bea
Long <longtranf@gmail.com>
2016-12-13 22:40:42 +0700

    Fix Get Paid Manager

--- src/controller/AnalysisController.java
+++ src/controller/AnalysisController.java
@@ -7,7 +7,6 @@ package controller;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import manage.*;
 import mediaone.*;
@@ -47,12 +46,15 @@ public class AnalysisController {
     }
     // End getter setter
 
+    // Get informantion for analysis
     public void getInformation(String start, String end) throws ParseException {
         Date startDate = changeToDate(start);
         Date endDate = changeToDate(end);
-        
+
         int DoanhThu = getPaidManager.getDoanhThu(startDate, endDate);
-        
+        int cost = getPaidManager.getChiPhi(startDate, endDate);
+        int profit = getPaidManager.getProfit(startDate, endDate);
+        analysisManagementFrame.loadData(DoanhThu, cost, profit, getPaidManager.listAllGet(startDate, endDate), getPaidManager.listAllPaided(startDate, endDate));
     }
 
     public Date changeToDate(String str) throws ParseException {
